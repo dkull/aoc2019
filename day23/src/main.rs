1a6f5ff01c9f77ad73d3d54cb28a0936d6ac384d
Tanel Liiv <liivtanel@gmail.com>
2020-01-03 07:57:00 +0200

    Day 23 part 2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -17,12 +17,15 @@ fn main() {
     for i in 0..proc_count {
         queue.insert(i as usize, vec![i]);
     }
+    queue.insert(255, vec![]);
 
+    let mut last_sent_to_zero = 0;
     loop {
+        let mut sent = false;
         for (net_id, proc) in (&mut processes).iter_mut().enumerate() {
-            println!("===");
-            println!("running machine {}", net_id);
+            println!("> {} [{}]", net_id, sent);
             for inp in queue.get(&net_id).unwrap().iter() {
+                sent = true;
                 println!("pushing {} to machine {}", *inp, net_id);
                 proc.input.push_back(*inp);
             }
@@ -36,16 +39,22 @@ fn main() {
                 let x = proc.output.pop_front().unwrap();
                 let y = proc.output.pop_front().unwrap();
                 if receiver == 255 {
-                    panic!("> {}", y);
+                    queue.get_mut(&receiver).unwrap().clear();
                 }
                 queue.get_mut(&receiver).unwrap().push(x);
                 queue.get_mut(&receiver).unwrap().push(y);
             }
-            //let proc_output = proc.output.pop_front();
-            //println!("{} sent: {:?}", net_id, proc_output);
         }
-        /*for inp in input_data.iter() {
-            proc.input.push_back(*inp as isize);
-        }*/
+        if !sent {
+            println!("NAT sending");
+            let y = queue.get_mut(&255).unwrap().pop().unwrap();
+            let x = queue.get_mut(&255).unwrap().pop().unwrap();
+            queue.get_mut(&0).unwrap().push(x);
+            queue.get_mut(&0).unwrap().push(y);
+            if y == last_sent_to_zero {
+                panic!("> p2: {}", y);
+            }
+            last_sent_to_zero = y;
+        }
     }
 }
